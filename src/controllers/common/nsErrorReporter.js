c18ee75b142610676573d8fec2d4d80a36ee51f7
interrupter <alex.repin@mail.ru>
2021-05-18 17:34:27 +0300

    added: nsErrorReporter now sets repoter origin to window.location.host

--- src/controllers/common/nsErrorReporter.js
+++ src/controllers/common/nsErrorReporter.js
@@ -4,6 +4,7 @@ class nsErrorReporter{
   constructor(app){
     this.app = app;
     this.reporter = new notErrorReporter();
+    this.reporter.setOrigin({ server: window.location.host });
   }
 
   report(e){
